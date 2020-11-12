f1a36e04699b9d2f4091398cc646b33694f0c8ec
Valentino Malvarmo <valentarmo@outlook.com>
2020-11-12 00:04:17 -0500

    Create Notebook from Python file

--- py2nb.py
+++ py2nb.py
@@ -1,18 +1,64 @@
 #!/usr/bin/env python3
+
 import sys
+import os
+import json
+
 from antlr4 import *
 from Py2NbLexer import Py2NbLexer
 from Py2NbParser import Py2NbParser
+from Py2NbListener import Py2NbListener
 
-def transform(input_):
-    # TODO: Actually Transform
-    input_stream = InputStream(input_)
+
+class NotebookWriter(Py2NbListener):
+
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.notebook = {
+            'cells': []
+        }
+
+    def enterDocument(self, ctx:Py2NbParser.DocumentContext):
+        pass
+
+    def exitDocument(self, ctx:Py2NbParser.DocumentContext):
+        with open(self.output_file, 'w') as nb:
+            json.dump(self.notebook, nb, indent=2)
+
+    def enterBlock(self, ctx:Py2NbParser.BlockContext):
+        cell = {
+            'cell_type': 'code',
+            'execution_count': 0,
+            'metadata': {},
+            'outputs': [],
+            'source': []
+        }
+        self.current_cell = cell
+
+    def exitBlock(self, ctx:Py2NbParser.BlockContext):
+        self.notebook['cells'].append(self.current_cell)
+
+    def exitStmt(self, ctx:Py2NbParser.StmtContext):
+        self.current_cell['source'].append(str(ctx.TEXT()))
+
+
+def transform(input_file, output_file):
+    input_stream = FileStream(input_file)
     lexer = Py2NbLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = Py2NbParser(stream)
     tree = parser.document()
-    print(tree.toStringTree(recog=parser))
+    walker = ParseTreeWalker()
+    notebook_writer = NotebookWriter(output_file)
+    walker.walk(notebook_writer, tree)
+
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        transform(f.read())
+    try:
+        input_file = sys.argv[1]
+        file_name, _ = os.path.splitext(input_file)
+        output_file = file_name + '.ipynb'
+        transform(input_file, output_file)
+    except Exception as e:
+        print(e)
+
